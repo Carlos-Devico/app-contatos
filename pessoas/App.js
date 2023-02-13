a5bcdaa73bdc02c8a7f53cbfc4a23a0eac476d17
carlos-devico <carlosdevico@gmail.com>
2023-02-13 11:52:43 -0300

    criado base do projeto pessoas

---
+++ pessoas/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View>
+        <Header />
+        <Header />
+        <Header />
+        <Header />
+      
+      </View>
+    );
+  }
+}
+
+
