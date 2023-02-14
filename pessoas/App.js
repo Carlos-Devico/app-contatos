ebe5a1780ef814824bce521aa6ddb580c3b31c69
carlos-devico <carlosdevico@gmail.com>
2023-02-14 02:42:04 -0300

    criado lista para renderizar nomes de uma array

--- pessoas/App.js
+++ pessoas/App.js
@@ -4,10 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
+ renderList(){
+  const names = [
+    'Edie',
+    'Paul',
+    'Snoopy'
+  ];
+
+  const textElements = names.map(x => {
+    return <Text>{x}</Text>
+  })
+  return textElements;
+ }
+
+
   render(){
     return (
       <View>
         <Header titulo="Pessoas"/>
+        { this.renderList() }
         
       </View>
     );
