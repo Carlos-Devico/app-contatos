93dfca6d1822ca6c4b9d6186ef49589e58072137
carlos-devico <carlosdevico@gmail.com>
2023-02-14 09:25:35 -0300

    criada promisses para requisições asincronas

--- pessoas/App.js
+++ pessoas/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
  renderList(){
-  const names = [
-    'Edie',
-    'Paul',
-    'Snoopy'
-  ];
+  /* Promisses */
+  axios.get('https://randomuser.me/api/?nat=br&results=5').then(response => {
+
+    const { results } = response.data
+    const names  = results.map(people => people.name.first);
+    console.log(names);
+    
+  })
 
   const textElements = names.map(x => {
     return <Text key={x}>{x}</Text>
