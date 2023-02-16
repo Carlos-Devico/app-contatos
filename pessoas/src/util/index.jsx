5eabb22056d5998794211a1ec63fd3457815e814
carlos-devico <carlosdevico@gmail.com>
2023-02-16 02:04:13 -0300

    alterado o export do index

--- pessoas/src/util/index.jsx
+++ pessoas/src/util/index.jsx
@@ -1,1 +1,6 @@
+// import capitalizeFirstLetter from './captalizeFirstLetter'
+
+// export {capitalizeFirstLetter};
+
+// import e export em uma linha
 export {default as capitalizeFirstLetter} from './captalizeFirstLetter'
